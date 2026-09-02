3a2e7dca60dc20b0735101c01b9605a8881afb5d
EgorSalahov <egorsalahov2@gmail.com>
2026-09-02 19:37:43 +0300

    Добавьте файлы проекта.

---
+++ dotnet-labs-bars/Program.cs
@@ -0,0 +1,5 @@
+﻿using dotnet_labs_bars;
+
+//StringReverseHomework.cs example
+Console.WriteLine(StringReverseHomework.ReverseWord("Привет"));
+Console.WriteLine(StringReverseHomework.ReverseSentence("Я люблю дотнет"));
---
+++ dotnet-labs-bars/StringReverseHomework.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_labs_bars
+{
+    public static class StringReverseHomework
+    {
+        public static string ReverseWord(string word)
+        {
+            return new string(word.ToArray().Reverse().ToArray());
+        }
+
+        public static string ReverseSentence(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = new string(words[i].ToArray().Reverse().ToArray());
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
